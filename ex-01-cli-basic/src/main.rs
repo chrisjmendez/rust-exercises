709e2e8454c0f48f10b64fa2766de015fba531e6
chrisaiv <support@chrisaiv.com>
2022-06-06 22:32:03 -0700

    simple clapp_app that will soon be deprecated

--- ex-01-cli-basic/src/main.rs
+++ ex-01-cli-basic/src/main.rs
@@ -1,3 +1,18 @@
+// Clap app is the main macro
+// Example: https://docs.rs/clap/3.0.0-beta.5/clap/macro.clap_app.html#examples
+
+use clap::{clap_app, crate_version};
+
 fn main() {
-    println!("Hello, world!");
+    let clap = clap_app!( app =>
+        (version:crate_version!())
+        (author:"Chris Mendez")
+        (about:"My First Exercise!")
+        (@arg input: +required "Sets the input file")
+    )
+    .get_matches();
+
+    println!("Input = {:?}", clap.value_of("input"));
+
+    println!("Complete");
 }
