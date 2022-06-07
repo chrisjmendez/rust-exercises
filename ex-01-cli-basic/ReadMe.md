38abe24a3ee31523d2517a816b518fd9f47cb0a1
chrisaiv <support@chrisaiv.com>
2022-06-07 10:40:55 -0700

    Exercise 01 uses a Builder Pattern-type approach to CLI

---
+++ ex-01-cli-basic/ReadMe.md
@@ -0,0 +1,14 @@
+# Description
+
+This example is referred to as 'Builder Pattern’ which allows more advanced configuration 
+options and dynamically generating arguments when desired. 
+
+# Getting Started
+```
+# cargo run -- <some pattern> <some file>
+cargo run -- MyFirstArg --config ReadMe.md --verbose 0
+```
+
+# Resources 
+* [Clap_v3 Docs](https://docs.rs/clap/3.0.0-beta.5/clap/index.html#features)
+* [Tutorial](https://rust-cli.github.io/book/tutorial/impl-draft.html)
--- ex-01-cli-basic/src/main.rs
+++ ex-01-cli-basic/src/main.rs
@@ -1,18 +1,67 @@
-// Clap app is the main macro
-// Example: https://docs.rs/clap/3.0.0-beta.5/clap/macro.clap_app.html#examples
-
-use clap::{clap_app, crate_version};
+#![allow(unused)]
+// Clap Command is the main macro
+// Example: https://docs.rs/clap/3.0.0-beta.5/clap/index.html#features
+use clap::{Arg, Command};
 
 fn main() {
-    let clap = clap_app!( app =>
-        (version:crate_version!())
-        (author:"Chris Mendez")
-        (about:"My First Exercise!")
-        (@arg input: +required "Sets the input file")
-    )
-    .get_matches();
 
-    println!("Input = {:?}", clap.value_of("input"));
+    let matches = Command::new("My CLI Command")
+        .version("1.0")
+        .author("Chris. <support@chrisaiv.com>")
+        .override_help("My First Command")
+        .arg(Arg::new("config")
+            .short('c')
+            .long("config")
+            .value_name("FILE")
+            .help("-c Sets a custom config file")
+            .takes_value(true))
+        .arg(Arg::new("INPUT")
+            .help("Sets the input file to use")
+            .required(true)
+            .index(1))
+        .arg(Arg::new("v")
+            .short('v')
+            .long("verbose")
+            .multiple_occurrences(true)
+            .takes_value(true)
+            .help("-v [0,1,2,-] Sets the level of verbosity"))
+        .subcommand(Command::new("test")
+            .override_help("controls testing features")
+            .version("1.3")
+            .author("Someone E. <someone_else@other.com>")
+            .arg(Arg::new("debug")
+                .short('d')
+                .help("print debug information verbosely")))
+        .get_matches();
 
-    println!("Complete");
+    // You can check the value provided by positional arguments, or option arguments
+    if let Some(i) = matches.value_of("INPUT") {
+        println!("Value for input: {}", i);
+    }
+
+    if let Some(c) = matches.value_of("config") {
+        println!("Value for config: {}", c);
+    }
+
+    // You can see how many times a particular flag or argument occurred
+    // Note, only flags can have multiple occurrences
+    match matches.occurrences_of("v") {
+        0 => println!("Verbose mode is off"),
+        1 => println!("Verbose mode is kind of on"),
+        2 => println!("Verbose mode is on"),
+        _ => println!("Don't be crazy"),
+    }
+
+    // You can check for the existence of subcommands, and if found use their
+    // matches just as you would the top level Command
+    if let Some(ref matches) = matches.subcommand_matches("test") {
+        // "$ myCommand test" was run
+        if matches.is_present("debug") {
+            // "$ myCommand test -d" was run
+            println!("Printing debug info...");
+        } else {
+            println!("Printing normally...");
+        }
+    }
+
 }
