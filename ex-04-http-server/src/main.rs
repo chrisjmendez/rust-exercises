bdb2489d0823cd214ada5ff33d871de6c63d7b71
chrisaiv <support@chrisaiv.com>
2022-06-08 21:59:17 -0700

    Added Server struct and impl block

--- ex-04-http-server/src/main.rs
+++ ex-04-http-server/src/main.rs
@@ -1,3 +1,31 @@
 fn main() {
-    println!("Hello, world!");
+
+    let server = Server::new("127.0.0.1:8080".to_string());
+        server.run();
+
+}
+
+// struct's resemble Class in OOP
+//   but unlike Classes, they do not include functionality.
+//   Instead, you add functionality to an implementation block.
+struct Server {
+    addr: String
+}
+
+// Two types of functionality within an implementation block.
+//   method: Similar to functions in OOP
+//   associated function: Similar to Static functions in OOP.
+impl Server {
+    // Use of "new" is simply a convention
+    // Self is an alias for its type
+    fn new(addr: String) -> Self {
+        Self {
+            // Syntactic sugar doesn't require an assignment
+            addr
+        }
+    }
+    // Run function takes ownership of the struct for this demo
+    fn run(self) {
+
+    }
 }
