e0c63494e7fe511fa309cc52ca9ca45fe0919999
chrisaiv <support@chrisaiv.com>
2022-06-29 16:20:52 -0700

    adding comments

--- ex-04-http-server/src/main.rs
+++ ex-04-http-server/src/main.rs
@@ -1,8 +1,7 @@
-fn main() {
 
+fn main() {
     let server = Server::new("127.0.0.1:8080".to_string());
         server.run();
-
 }
 
 // struct's resemble Class in OOP
@@ -13,19 +12,27 @@ struct Server {
 }
 
 // Two types of functionality within an implementation block.
-//   method: Similar to functions in OOP
-//   associated function: Similar to Static functions in OOP.
+//   1. method: Similar to functions in OOP
+//   2. associated function: Similar to Static functions in OOP.
 impl Server {
-    // Use of "new" is simply a convention
-    // Self is an alias for its type
+    // A. Use of "new" is simply a convention
+    // B. Self is an alias for its type so "Self" and "Server" are interchangable
     fn new(addr: String) -> Self {
-        Self {
-            // Syntactic sugar doesn't require an assignment
+        Server {
+            // C. Syntactic sugar doesn't require an assignment addr:̶a̶d̶d̶r̶
             addr
         }
     }
-    // Run function takes ownership of the struct for this demo
-    fn run(self) {
 
+    // C. Run function takes ownership of the struct for this demo
+    fn run(self) {
+        println!("Listening on {}", self.addr)
     }
+}
+
+// Rust enums are similar to the algabraic data-types in Haskell
+struct Request {
+    path: String,
+    query_string: String,
+    method: String
 }
