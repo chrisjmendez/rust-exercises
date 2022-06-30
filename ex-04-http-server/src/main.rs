93e88df6431a40c176e6de9d0505d05eba1240ee
chrisaiv <support@chrisaiv.com>
2022-06-29 17:11:32 -0700

    Refactored code into modules

--- ex-04-http-server/src/main.rs
+++ ex-04-http-server/src/main.rs
@@ -1,55 +1,59 @@
+use http::request::Request;
+use server::Server;
 
 fn main() {
-    let get    = Method::GET;
-    let delete = Method::DELETE;
-    let post   = Method::POST;
-    let put    = Method::PUT;
-
     let server = Server::new("127.0.0.1:8080".to_string());
         server.run();
 }
 
-// struct's resemble Class in OOP
-//   but unlike Classes, they do not include functionality.
-//   Instead, you add functionality to an implementation block.
-struct Server {
-    addr: String
+mod server {
+    // struct's resemble Class in OOP
+    //  but unlike Classes, they do not include functionality.
+    //  Instead, you add functionality to an implementation block.
+    pub struct Server {
+        addr: String
+    }
+
+    // Two types of functionality within an implementation block.
+    //   1. method: Similar to functions in OOP
+    //   2. associated function: Similar to Static functions in OOP.
+    impl Server {
+        // A. Use of "new" is simply a convention
+        // B. Self is an alias for its type so "Self" and "Server" are interchangable
+        pub fn new(addr: String) -> Self {
+            // C. Syntactic sugar doesn't require an assignment addr:̶a̶d̶d̶r̶
+            Server { addr }
+        }
+
+        // D. Run function takes ownership of the struct for this demo
+        pub fn run(self) {
+            println!("Listening on {}", self.addr)
+        }
+    }
 }
 
-// Two types of functionality within an implementation block.
-//   1. method: Similar to functions in OOP
-//   2. associated function: Similar to Static functions in OOP.
-impl Server {
-    // A. Use of "new" is simply a convention
-    // B. Self is an alias for its type so "Self" and "Server" are interchangable
-    fn new(addr: String) -> Self {
-        Server {
-            // C. Syntactic sugar doesn't require an assignment addr:̶a̶d̶d̶r̶
-            addr
+// HTTP Module
+mod http {
+    pub mod request {
+        use super::method::Method;
+        // Rust enums are similar to the algabraic data-types in Haskell
+        pub struct Request {
+            path: String,
+            query_string: Option<String>, //Option functions like Null in a type-safe way
+            method: Method,
         }
     }
 
-    // D. Run function takes ownership of the struct for this demo
-    fn run(self) {
-        println!("Listening on {}", self.addr)
+    pub mod method {
+        pub enum Method {
+            GET,
+            DELETE,
+            POST,
+            PUT,
+            CONNECT,
+            OPTIONS,
+            TRACE,
+            PATCH
+        }
     }
-}
-
-// Rust enums are similar to the algabraic data-types in Haskell
-struct Request {
-    path: String,
-    // This is how, in Rust, you can introduce Null in a type-safe way
-    query_string: Option<String>,
-    method: Method
-}
-
-enum Method {
-    GET,
-    DELETE,
-    POST,
-    PUT,
-    CONNECT,
-    OPTIONS,
-    TRACE,
-    PATCH
 }
